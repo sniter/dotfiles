a732fce67fee46713d7b0d7f03091b7fe3e1c1d1
Ilya Babich <sniter@gmail.com>
2024-03-06 22:15:05 +0200

    Nvim telescope config

--- nvim/.config/nvim/lua/bootstrap/opts.lua
+++ nvim/.config/nvim/lua/bootstrap/opts.lua
@@ -1,4 +1,4 @@
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
 -- Color Scheme
-vim.cmd.colorscheme("desert")
+vim.cmd.colorscheme("slate")
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -18,12 +18,18 @@ return {
 						},
 					},
 				},
+                -- Enable to search hidden files
+                -- pickers = {
+                --     find_files = {
+                --         hidden = true
+                --     }
+                -- },
 			})
 
 			-- Enable telescope fzf native, if installed
 			pcall(require("telescope").load_extension, "fzf")
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
