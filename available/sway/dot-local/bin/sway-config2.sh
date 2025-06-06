a707041690a3315f1cb8e531532f775e434e6e2b
Ilya Babich <sniter@gmail.com>
2025-06-06 16:40:55 +0300

    sway status updated

--- available/sway/dot-local/bin/sway-config2.sh
+++ available/sway/dot-local/bin/sway-config2.sh
@@ -5,21 +5,19 @@ BATTERY_CHARGING=""
 BATTERY_FULL=""
 BATTERY_NOT_CHARGING=""
 BATTERY_DISCHARGING=""
-TIME_FMT='%Y-%m-%d %H:%M'
+TIME_FMT='%a %b %d %H:%M'
+# TIME_FMT='%Y-%m-%d %T'
 MEMORY_LABEL=" mem %d%%"
 CPU_LABEL=" cpu %d%%"
 DISK_LABEL=" dsk %d%%"
-SWAP_LABEL="/%d%%"
+SWAP_LABEL="/%d%% "
 TEMP_LABEL=" temp %s"
-LAYOUT_LABEL=" | "
-BATTERY_LABEL=" %s%s %d%%"
-CALENDAR_LABEL=" | %s"
+LAYOUT_LABEL=" %s "
+BATTERY_LABEL=" %s%s %d%% "
+CALENDAR_LABEL=" %s"
 WEATHER_LOCATION="Jurmala"
 WEATHER_FMT="%C+%t+%w"
-WEATHER_LABEL=" | %s"
-
-BATTERY=$(cat /sys/class/power_supply/BAT0/capacity 2>/dev/null)
-BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status)
+WEATHER_LABEL=" %s "
 
 weather() {
   local ttl=1200 # Default TTL is 20 mins
@@ -31,15 +29,22 @@ weather() {
     curl -s "wttr.in/${WEATHER_LOCATION:-Riga}?format=${WEATHER_FMT:-1}" >"$cache_file"
   fi
 
-  printf "${WEATHER_LABEL:- WEATHER: %s}" "$(cat $cache_file)" "$needs_refresh"
+  printf "${WEATHER_LABEL:- WEATHER: %s}" "$(cat $cache_file)"
 }
 
-layout() {
-  LAYOUT=$(
-    swaymsg -t get_inputs | jq '.[0].xkb_active_layout_name' | cut -d '"' -f 2 2>/dev/null
-  )
+weather_render() {
+  jq -n -c \
+    --arg name "Layout" \
+    --arg full_text "$(weather)" \
+    '{name:$name,full_text:$full_text}'
+}
 
-  printf "${LAYOUT_LABEL}%s" $(case "$LAYOUT" in
+#
+# LAYOUT
+#
+LAYOUT="EN"
+layout() {
+  echo $(case "$1" in
     Rus*)
       echo "RU"
       ;;
@@ -50,9 +55,21 @@ layout() {
       echo "EN"
       ;;
     *)
-      echo "${1:0:2}"
+      echo "${layout:0:2}"
       ;;
     esac)
+}
+layout_init() {
+  LAYOUT="$(layout $(swaymsg -t get_inputs | jq '.[].xkb_active_layout_name' | grep -v null | head -n1 | cut -d '"' -f 2))"
+}
+layout_update() {
+  LAYOUT="$(layout $(echo "$1" | jq '.input.xkb_active_layout_name' | cut -d '"' -f 2)) "
+}
+layout_render() {
+  jq -n -c \
+    --arg name "Layout" \
+    --arg full_text "$(printf "${LAYOUT_LABEL:- Layout: %s}" "$LAYOUT")" \
+    '{name:$name,full_text:$full_text}'
 }
 
 cpu() {
@@ -64,8 +81,9 @@ disk() {
 }
 
 battery_icn() {
-  len="${#BATTERY_ICONS[@]}"
-  idx=$(printf "%d" $(("$BATTERY" / "$len")))
+  local level="$1"
+  local len="${#BATTERY_ICONS[@]}"
+  local idx=$(printf "%d" $(("$level" / "$len")))
   if [ "$idx" -lt "$len" ]; then
     echo "${BATTERY_ICONS[$idx]}"
   else
@@ -74,6 +92,7 @@ battery_icn() {
 }
 
 battery_status() {
+  local BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status)
   case "$BATTERY_STATUS" in
   Charging)
     echo "$BATTERY_CHARGING"
@@ -94,7 +113,16 @@ battery_status() {
 }
 
 battery() {
-  printf "${BATTERY_LABEL:-BAT: %s%s %d%%}" "$(battery_status)" "$(battery_icn)" "$BATTERY"
+  local level="$1"
+  printf "${BATTERY_LABEL:-BAT: %s%s %d%%}" "$(battery_status)" "$(battery_icn "$level")" "$level"
+}
+
+battery_render() {
+  local level=$(cat /sys/class/power_supply/BAT0/capacity 2>/dev/null)
+  jq -n -c \
+    --arg name "Battery" \
+    --arg full_text "$(battery "$level")" \
+    '{name: $name, full_text: $full_text}'
 }
 
 temperature() {
@@ -109,8 +137,77 @@ swap() {
   printf "${SWAP_LABEL:-SWP: %d%%}" $(free | grep "Swap" | awk '{print $3/$2*100}')
 }
 
-calendar() {
-  printf "${CALENDAR_LABEL:- %s}" "$(date +"$TIME_FMT")"
+stats_render() {
+  jq -n -c \
+    --arg name "Laptop stats" \
+    --arg full_text "$(temperature)$(cpu)$(disk)$(memory)$(swap)" \
+    '{name: $name, full_text: $full_text}'
 }
 
-echo "$(temperature)$(disk)$(cpu)$(memory)$(swap)$(weather)$(layout)$(battery)$(calendar)"
+#
+# CALENDAR
+#
+calendar_render() {
+  jq -n -c \
+    --arg name "Time" \
+    --arg full_text "$(printf "${CALENDAR_LABEL:- %s}" "$(date +"$TIME_FMT")")" \
+    '{name: $name, full_text: $full_text}'
+}
+
+#
+# STATUS RENDER
+#
+render() {
+  echo "[$(stats_render),$(weather_render),$(layout_render),$(battery_render),$(calendar_render)],"
+}
+
+render_error() {
+  local msg=$1
+  local error=$(
+    jq -n -c \
+      --arg name Error \
+      --arg full_text "$msg" \
+      '{name: $name, full_text: $full_text}'
+  )
+  echo "[$error],"
+}
+
+ticking() {
+  while true; do
+    swaymsg -t send_tick
+    sleep 5 # пауза 1 секунда
+  done &
+}
+
+main() {
+  # ticking
+  # 1>/dev/null 2>/dev/null
+  echo '{"version": 1, "click_events": false}'
+  echo "["
+  # set -x
+
+  swaymsg -t subscribe -m '["tick","input"]' -r | while read -r event; do
+    case $(echo "$event" | jq -r '.change // .first' | cut -d '"' -f 2) in
+    true)
+      layout_init
+      ;&
+    false)
+      echo "$(render)"
+      ;;
+    xkb_layout)
+      layout_update "$event"
+      echo "$(render)"
+      ;;
+    libinput_config | xkb_keymap) ;;
+    *)
+      echo "$(render_error "$(echo "$event" | jq -r '.first // .change')")"
+      ;;
+    esac
+  done
+
+  # set +x
+  echo "[],]"
+}
+
+main
+# echo "$(temperature)$(disk)$(cpu)$(memory)$(swap)$(weather)$(layout)$(battery)$(calendar)"
