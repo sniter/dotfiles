e4eec851c21ae571ff861ac2ed75097370be5cde
Ilya Babich <sniter@gmail.com>
2025-10-26 17:43:43 +0200

    fixing makefiles

--- config.mk
+++ config.mk
@@ -10,11 +10,6 @@ COMMON_TERMINALS=alacitty kitty ghostty
 
 GIT_EMAIL=$(shell echo c25pdGVyQGdtYWlsLmNvbQo= | base64 -d)
 
-# common/ssh/github:
-# 	mkdir -p ~/.ssh/github.com/git
-# 	ssh-keygen -t ed25519 -C "sniter@gmail.com" -f ~/.ssh/github.com/git/id_ed25519
-# 	cat ~/.ssh/github.com/git/id_ed25519.pub
-
 SSH_KEYGEN=\
 	mkdir -p ~/.ssh/$(1)/$(2) && \
 	ssh-keygen -t $(3) -C "$(4)" -f ~/.ssh/$(1)/$(2)/id_$(3) && \
