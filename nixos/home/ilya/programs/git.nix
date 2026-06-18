208bbef51cf0baf1de3fb8451515daafd939e771
Ilya Babich <sniter@gmail.com>
2026-06-18 22:37:01 +0300

    NixOS: git config updated

--- nixos/home/ilya/programs/git.nix
+++ nixos/home/ilya/programs/git.nix
@@ -1,28 +1,64 @@
 { ... }:
 
 {
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
 
-  programs.git.settings = {
-
-    user = {
-    	name = "Ilya Babich";
+    settings = {
+      user = {
+        name = "Ilya Babich";
         email = "sniter@gmail.com";
-    };
+      };
 
-    init.defaultBranch = "main";
-    pull.rebase = true;
-    rebase.autoStash = true;
-    core.editor = "nvim";
-    diff.colorMoved = "default";
-    merge.conflictstyle = "diff3";
+      init.defaultBranch = "main";
 
-    alias = {
-      st = "status";
-      co = "checkout";
-      br = "branch";
-      cm = "commit";
-      lg = "log --oneline --graph --decorate --all";
+      core = {
+        autocrlf = "input";
+        pager = "less -S";
+        editor = "nvim";
+      };
+
+      push.default = "matching";
+      pull.rebase = true;
+      rebase.autoStash = true;
+
+      diff = {
+        algorithm = "patience";
+        compactionHeuristic = true;
+        colorMoved = "default";
+        tool = "nvimdiff";
+      };
+
+      merge = {
+        tool = "nvimdiff4";
+        prompt = false;
+        conflictstyle = "diff3";
+      };
+
+      color = {
+        diff = "auto";
+        status = "auto";
+        branch = "auto";
+        interactive = "auto";
+        ui = true;
+        pager = true;
+      };
+
+      alias = {
+        st = "status";
+        co = "checkout";
+        br = "branch";
+        cm = "commit";
+        lg  = "!git lg1";
+        lg1 = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+        lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
+        lg3 = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+        commit-merge = "!head -1 $(git rev-parse --git-dir)/MERGE_MSG | git commit -F -";
+      };
+
+      difftool.nvimdiff.cmd = ''nvim -d "$LOCAL" "$REMOTE" -c "wincmd w" -c "wincmd L"'';
+
+      mergetool.nvimdiff4.cmd = "nvim -d $LOCAL $BASE $REMOTE $MERGED -c '$wincmd w' -c 'wincmd J'";
     };
   };
 }
