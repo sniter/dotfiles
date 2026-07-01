500f6bf219efbc3d2a865637299556712a9e7d9a
Ilya Babich <sniter@gmail.com>
2026-07-01 18:37:18 +0300

    Updated steam config

--- nixos/modules/services/steam.nix
+++ nixos/modules/services/steam.nix
@@ -17,7 +17,26 @@
 
   environment.systemPackages = with pkgs; [
     protonup-qt
+    mangohud
   ];
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      libva-vdpau-driver
+      libvdpau-va-gl
+    ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      intel-media-driver
+      libva-vdpau-driver
+    ];
+  };
 
 }
 
